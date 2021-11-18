0a55c0d5518d88ceb3e14eec99883d5b4881784f
Michael Orman <michaelorman61@gmail.com>
2021-11-17 19:37:26 -0500

    Initial commit

---
+++ server/api/users.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+
+// router.get('/:userId', async (req, res, next) => {
+//   // try{
+//   // } catch {
+//   //   console.log('THERE WAS AN ERROR');
+//   // }
+// })
+
