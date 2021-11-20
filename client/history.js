1c16271c2d3fd0dbb024b6d1a493b677e78f2773
Michael Orman <michaelorman61@gmail.com>
2021-11-19 23:07:50 -0500

    added backend support for user authentication -> still need to add frontend routing and redux store

---
+++ client/history.js
@@ -0,0 +1,10 @@
+
+
+import {createMemoryHistory, createBrowserHistory} from 'history'
+
+const history =
+  process.env.NODE_ENV === 'test'
+    ? createMemoryHistory()
+    : createBrowserHistory()
+
+export default history
