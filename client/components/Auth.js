fecd560f2ac44f736213d6c7df3003a973cd3535
Michael Orman <michaelorman61@gmail.com>
2021-11-23 18:44:39 -0500

    fixed bug regarding regeneratorRuntime for log in and sign up (think it had something to do with async functions in redux store and babel-preset-env)

--- client/components/Auth.js
+++ client/components/Auth.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import regeneratorRuntime from 'regenerator-runtime';
 import { authenticate } from '../redux/auth'
 
 const Auth = (props) => {
